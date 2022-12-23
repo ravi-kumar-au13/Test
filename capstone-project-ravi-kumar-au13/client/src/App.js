c123ccf634b4684193595ed9381d2278267f882a
Ravi Kumar <ravi89638@gmail.com>
2022-12-23 09:30:17 +0530

    Capstone-project-ravi-kumar-au3

---
+++ capstone-project-ravi-kumar-au13/client/src/App.js
@@ -0,0 +1,83 @@
+import React from 'react';
+import { Route, BrowserRouter} from 'react-router-dom';
+import CartScreen from './screens/cartScreen';
+import HomeScreen from './screens/HomeScreen';
+import ProductScreen from './screens/ProductScreen';
+import { Link } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+import SigninScreen from './screens/SigninScreen';
+import RegisterScreen from './screens/RegisterScreen';
+import ShippingAddressScreen from './screens/ShippingAddressScreen';
+import PaymentMethodScreen from './screens/PaymentMethodScreen';
+import PlaceOrderScreen from './screens/PlaceOrderScreen';
+import OrderScreen from './screens/OrderScreen';
+import { signout } from './actions/userActions';
+
+function App() {
+
+  const cart = useSelector((state) => state.cart);
+  const { cartItems } = cart;
+  const userSignin = useSelector((state) => state.userSignin);
+  const { userInfo } = userSignin;
+  const dispatch = useDispatch();
+  const signoutHandler = () => {
+    dispatch(signout());
+  };
+
+  return (
+    <BrowserRouter>
+    <div className="grid-container">
+      <header className="row">
+        <div>
+          <Link className="brand" to="/">shopaholic</Link>
+        </div>
+        <div>
+          <Link to="/cart">
+          Cart
+          {cartItems.length > 0 && (
+            <span className="badge">{cartItems.length}</span>
+          )}
+          </Link>
+
+          
+            {userInfo ? (
+              <div className="dropdown">
+                <Link to="#">
+                  {userInfo.name} <i className="fa fa-caret-down"></i>{' '}
+                </Link>
+                <ul className="dropdown-content">
+                  <li>
+                    <Link to="#signout" onClick={signoutHandler}>
+                      Sign Out
+                    </Link>
+                  </li>
+                </ul>
+              </div>
+            ) : (
+              <Link to="/signin">Sign In</Link>
+            )}
+        </div>
+      </header>
+
+      <main>
+        
+        <Route path="/cart/:id?" component={CartScreen}></Route>  
+        <Route path="/product/:id" component={ProductScreen}></Route>
+        <Route path="/" component={HomeScreen} exact></Route>
+        <Route path="/signin" component={SigninScreen}></Route>
+        <Route path="/register" component={RegisterScreen}></Route>
+        <Route path="/shipping" component={ShippingAddressScreen}></Route>
+        <Route path="/payment" component={PaymentMethodScreen}></Route>
+        <Route path="/placeorder" component={PlaceOrderScreen}></Route>
+        <Route path="/order/:id" component={OrderScreen}></Route>
+
+      </main>
+
+      <footer className="row center">All rights reserved</footer>
+
+    </div>
+    </BrowserRouter>
+  );
+}
+
+export default App;
